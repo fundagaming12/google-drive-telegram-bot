5780877c4700b899f33857f11b1203b9ba68a146
Himanshusinhahzb <88733697+Himanshusinhahzb@users.noreply.github.com>
2021-08-11 15:59:24 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,7 @@ class config:
     SUDO_USERS = "1945481931" # Sepearted by space.
     SUPPORT_CHAT_LINK =""
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
+    G_DRIVE_CLIENT_ID = "https://www.googleapis.com/auth/drive.file"
     G_DRIVE_CLIENT_SECRET = ""
 
 
